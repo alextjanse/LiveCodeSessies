a7c8b54031d3ae48fda2d3f95e719d5409763272
alextjanse <github@alexjanse.nl>
2025-10-28 11:03:15 +0100

    boolean update lazy example

--- Booleans/lazy_checks.js
+++ Booleans/lazy_checks.js
@@ -1,9 +1,20 @@
-const l = null;
+let l = { value: 1 };
+// l = null; // (un)-comment to see difference
 
-if (l && l.value == 1) {
-    console.log('not-null check first');
+// Nested
+if (l) {
+    console.log('l is not null');
+    if (l.value === 1) {
+        console.log('value is 1');
+    }
 }
 
-if (l.value == 1 && l) {
+// Left side is evaluated first, right side only when necessary
+if (l && l.value === 1) {
+    console.log('l is not null, value is 1');
+}
+
+// Left side is evaluated first, crashes if undefined
+if (l.value === 1 && l) {
     console.log('crash: l.value undefined')
 }
